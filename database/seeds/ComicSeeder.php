b34ffb8e4c42a32f18f204b0bd77b0f5a1693adc
PietroDanieli <1pie7d@gmail.com>
2022-11-08 10:27:34 +0100

    complete >.<

---
+++ database/seeds/ComicSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Comic;
+use Illuminate\Database\Seeder;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $comics = config('data.comics');
+        foreach($comics as $model){
+            $comic = new Comic();
+
+            $comic['title'] = $model['title'];
+            $comic['description'] = $model['description'];
+            $comic['thumb'] = $model['thumb'];
+            $comic['price'] = $model['price'];
+            $comic['series'] = $model['series'];
+            $comic['sale_date'] = $model['sale_date'];
+            $comic['type'] = $model['type'];
+
+            $comic->save();
+        }
+    }
+}
