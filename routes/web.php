6af0b89eaf49e5dcb665f8ffc92233b17187f112
PietroDanieli <1pie7d@gmail.com>
2022-11-09 07:51:13 +0100

    redirect principal route

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ComicController@index'  )->name('index');
+
+
 Route::resource('comics', 'ComicController');
