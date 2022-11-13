4adecf780b7aa5432e55a8297be71c95d09540f1
PietroDanieli <1pie7d@gmail.com>
2022-11-13 15:08:46 +0100

    ability css

--- resources/views/comics/layouts/layout.blade.php
+++ resources/views/comics/layouts/layout.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/app.js') }}"></script> --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}"></script>
     <title>Laravel-base-crud</title>
 </head>
 
